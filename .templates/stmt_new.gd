4c5b979b663c99f0a7c550d9ef34ec789bfd8419
Liam Wofford <27900656+nulture@users.noreply.github.com>
2024-10-04 13:14:47 -0500

    Add stmt template file

---
+++ .templates/stmt_new.gd
@@ -0,0 +1,27 @@
+
+## No description
+class_name StmtNew extends Stmt
+
+func _get_is_halting() -> bool:
+	return super._get_is_halting()
+
+func _get_keyword() -> StringName:
+	return super._get_keyword()
+
+func _get_verbosity() -> int:
+	return super._get_verbosity()
+
+# func _load() -> void:
+# 	super._load()
+
+func _execute(host: PennyHost) -> Record:
+	return super._execute(host)
+
+# func _undo(record: Record) -> void:
+# 	super._undo(record)
+
+func _message(record: Record) -> Message:
+	return super._message(record)
+
+func _validate() -> PennyException:
+	return super._validate()
