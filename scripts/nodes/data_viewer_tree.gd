99e559151bd73983bbc4652db56f59d3609cf522
Liam Wofford <27900656+nulture@users.noreply.github.com>
2024-10-16 16:39:44 -0500

    Added clear button to data viewer search bar

--- scripts/nodes/data_viewer_tree.gd
+++ scripts/nodes/data_viewer_tree.gd
@@ -3,6 +3,7 @@ class_name DataViewerTree extends Tree
 
 @export var debug : PennyDebug
 
+
 var root : TreeItem
 var _query : String
 var query : String :
@@ -11,6 +12,7 @@ var query : String :
 		_query = value
 		refresh_search()
 
+
 var _search_values : bool = true
 var search_values : bool = true :
 	get: return _search_values
@@ -18,12 +20,14 @@ var search_values : bool = true :
 		_search_values = value
 		refresh_search()
 
+
 var _sort_method := PennyObject.Sort.NONE
 var sort_method := PennyObject.Sort.NONE :
 	get: return _sort_method
 	set (value):
 		_sort_method = value
 		refresh()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,8 +42,10 @@ func _ready() -> void:
 
 	setup.call_deferred()
 
+
 func setup() -> void:
 	debug.host.on_data_modified.connect(refresh)
+
 
 func refresh() -> void:
 	self.clear()
@@ -50,12 +56,14 @@ func refresh() -> void:
 
 	refresh_search()
 
+
 func refresh_search() -> void:
 	if query == "":
 		set_visible_recursive(root, true)
 	else:
 		for i in root.get_children():
 			set_visible_if_matched_query(i, query)
+
 
 func set_visible_if_matched_query(item: TreeItem, __query: String) -> bool:
 	var result := item.get_text(0).containsn(query) or (search_values and item.get_text(1).containsn(query))
@@ -67,18 +75,24 @@ func set_visible_if_matched_query(item: TreeItem, __query: String) -> bool:
 		item.visible = result
 	return result
 
+
 func set_visible_recursive(item: TreeItem, value: bool) -> void:
 	item.visible = value
 	for i in item.get_children():
 		set_visible_recursive(i, value)
 
-func _on_query_values_toggle_toggled(toggled_on:bool) -> void:
+
+func _on_search_values_toggle_toggled(toggled_on:bool) -> void:
 	search_values = toggled_on
 
 
-func _on_data_search_bar_text_changed(new_text:String) -> void:
+func _on_search_bar_text_changed(new_text:String) -> void:
 	query = new_text
 
 
 func _on_sort_selector_item_selected(index:int) -> void:
 	sort_method = index as PennyObject.Sort
+
+
+func _on_search_clear_pressed() -> void:
+	query = ""
