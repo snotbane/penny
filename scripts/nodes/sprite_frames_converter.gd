19b4b7b8cf958d5cdecda26d3f59afc907d8b72a
Liam Wofford <27900656+nulture@users.noreply.github.com>
2024-12-05 13:36:03 -0600

    Updated sprite_frames_converter to be non-destructive

--- scripts/nodes/sprite_frames_converter.gd
+++ scripts/nodes/sprite_frames_converter.gd
@@ -4,61 +4,39 @@ class_name SpriteFramesConverter extends Node
 
 @export var create_reset : bool :
 	set(value):
-		if player.has_animation("RESET"):
-			library_default.remove_animation("RESET")
-
-		var anim := Animation.new()
-
+		var anim_name := "RESET"
+		var anim := get_or_create_anim(anim_name)
 		anim.length = 0.0
 
-		anim.add_track(Animation.TrackType.TYPE_VALUE, 0)
-		anim.value_track_set_update_mode(0, Animation.UpdateMode.UPDATE_DISCRETE)
-		anim.track_set_path(0, ^".:sprite_frames")
-		anim.track_insert_key(0, 0.0, sprite.sprite_frames)
+		var sprite_track := get_and_clear_track(anim, ^".:sprite_frames")
+		anim.track_insert_key(sprite_track, 0.0, sprite.sprite_frames)
 
-		anim.add_track(Animation.TrackType.TYPE_VALUE, 1)
-		anim.value_track_set_update_mode(1, Animation.UpdateMode.UPDATE_DISCRETE)
-		anim.track_set_path(1, ^".:animation")
-		anim.track_insert_key(1, 0.0, sprite.animation)
+		var name_track := get_and_clear_track(anim, ^".:animation")
+		anim.track_insert_key(name_track, 0.0, sprite.animation)
 
-		anim.add_track(Animation.TrackType.TYPE_VALUE, 2)
-		anim.value_track_set_update_mode(2, Animation.UpdateMode.UPDATE_DISCRETE)
-		anim.track_set_path(2, ^".:frame")
-		anim.track_insert_key(2, 0.0, 0)
-
-		library_default.add_animation("RESET", anim)
+		var frame_track := get_and_clear_track(anim, ^".:frame")
+		anim.track_insert_key(frame_track, 0.0, 0)
 
 @export var create_anims : bool :
 	set(value):
 		for anim_name in sprite.sprite_frames.get_animation_names():
-			if player.has_animation(anim_name):
-				library_default.remove_animation(anim_name)
+			var anim := get_or_create_anim(anim_name)
 
-			var anim := Animation.new()
+			var name_track := get_and_clear_track(anim, ^".:animation")
+			anim.track_insert_key(name_track, 0.0, anim_name)
 
+			var frame_track := get_and_clear_track(anim, ^".:frame")
+			var speed := 1.0 / sprite.sprite_frames.get_animation_speed(anim_name)
+			var cursor := 0.0
+			for i in sprite.sprite_frames.get_frame_count(anim_name):
+				anim.track_insert_key(frame_track, cursor, i)
+				cursor += sprite.sprite_frames.get_frame_duration(anim_name, i) * speed
+
+			anim.length = cursor
 			if sprite.sprite_frames.get_animation_loop(anim_name):
 				anim.loop_mode = Animation.LOOP_LINEAR
 			else:
 				anim.loop_mode = Animation.LOOP_NONE
-
-			anim.add_track(Animation.TrackType.TYPE_VALUE, 0)
-			anim.value_track_set_update_mode(0, Animation.UpdateMode.UPDATE_DISCRETE)
-			anim.track_set_path(0, ^".:animation")
-			anim.track_insert_key(0, 0.0, anim_name)
-
-			anim.add_track(Animation.TrackType.TYPE_VALUE, 1)
-			anim.value_track_set_update_mode(1, Animation.UpdateMode.UPDATE_DISCRETE)
-			anim.track_set_path(1, ^".:frame")
-
-			var speed := 1.0 / sprite.sprite_frames.get_animation_speed(anim_name)
-			var cursor := 0.0
-			for i in sprite.sprite_frames.get_frame_count(anim_name):
-				anim.track_insert_key(1, cursor, i)
-				cursor += sprite.sprite_frames.get_frame_duration(anim_name, i) * speed
-
-			anim.length = cursor
-
-			library_default.add_animation(anim_name, anim)
 
 
 @onready var sprite : AnimatedSprite2D = self.get_parent().get_parent()
@@ -75,3 +53,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func get_or_create_anim(anim_name : StringName) -> Animation:
+	var anim : Animation
+	if player.has_animation(anim_name):
+		anim = player.get_animation(anim_name)
+	else:
+		anim = Animation.new()
+		library_default.add_animation(anim_name, anim)
+	return anim
+
+
+static func get_and_clear_track(anim: Animation, path: NodePath, type : int = Animation.TrackType.TYPE_VALUE, update : int = Animation.UpdateMode.UPDATE_DISCRETE) -> int:
+	var track := anim.find_track(path, type)
+	if track == -1:
+		track = anim.add_track(type)
+		anim.value_track_set_update_mode(track, update)
+		anim.track_set_path(track, path)
+	while anim.track_get_key_count(track) > 0:
+		anim.track_remove_key(track, 0)
+	return track
