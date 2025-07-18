cbac474f394832f56a05e54faecd4de7a41184b3
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-07-18 01:38:23 -0500

    Fixup

--- assets/scripts/decor/decor_registry.gd
+++ assets/scripts/decor/decor_registry.gd
@@ -2,7 +2,7 @@
 ## Super simple class that ensures [Deco]s get registered properly. If you wish to use custom decors, they must be added to one.
 class_name DecorRegistry extends Resource
 
-@export var decors : Array
+@export var decors : Array[Decor]
 
 func register_all_decors() -> void:
 	for decor in decors:
