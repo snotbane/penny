8167fb77069d160b29dd5c3eff8637c48eb2897a
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-07-21 09:09:04 -0500

    Made instanceable DialogBubble3Ds

--- assets/scenes/dialog_elements/bubbles/DialogBubble3D.gd
+++ assets/scenes/dialog_elements/bubbles/DialogBubble3D.gd
@@ -3,7 +3,7 @@ extends Node3D
 class_name DialogBubble3D
 
 @export_tool_button("Refresh") var refresh_button_ := refresh
-@export_tool_button("Rebuild Viewport Cache") var rebuild_viewport_cache_ := rebuild_viewport_cache
+# @export_tool_button("Rebuild Viewport Cache") var rebuild_viewport_cache_ := rebuild_viewport_cache
 
 ## Determines how the bubble grows in size. Domain is the total number of characters in the text, range is the width.
 @export var size_curve_x : Curve
@@ -16,57 +16,6 @@ class_name DialogBubble3D
 
 @export_range(2, 2048) var viewport_pixels_per_unit : int = 100
 
-#region Static Viewport Data
-@export_subgroup("Static")
-
-static var viewport_cache : Texture2DArray :
-	get: return RenderingServer.global_shader_parameter_get(&"bubble_cache")
-	set(value):
-		RenderingServer.global_shader_parameter_set(&"bubble_cache", value)
-
-# static var _viewports_used : int
-
-static var _max_viewport_size : int = 2048
-@export var max_viewport_size : int = 2048 :
-	get: return _max_viewport_size
-	set(value):
-		if _max_viewport_size == value: return
-		_max_viewport_size = value
-		rebuild_viewport_cache()
-
-static var _max_bubbles : int = 8
-@export_range(0, 63) var max_bubbles : int = 8 :
-	get: return _max_bubbles
-	set(value):
-		if _max_bubbles == value: return
-		_max_bubbles = value
-		rebuild_viewport_cache()
-
-static func create_viewport_cache_layer() -> Image:
-	return Image.create_empty(_max_viewport_size, _max_viewport_size, false, Image.FORMAT_RGBA8)
-
-static func rebuild_viewport_cache() -> void:
-	var images : Array[Image] = []
-	images.resize(_max_bubbles)
-	for i in _max_bubbles:
-		images[i] = create_viewport_cache_layer()
-	viewport_cache = Texture2DArray.new()
-	viewport_cache.create_from_images(images)
-
-var _bubble_id : int = 0
-@export var bubble_id : int = 0 :
-	get: return _bubble_id
-	set(value):
-		value = clampi(value, 0, _max_bubbles - 1)
-		if _bubble_id == value: return
-		_bubble_id = value
-		text_mesh.set_instance_shader_parameter(&"bubble_id", _bubble_id)
-
-static func request_new_bubble_id() -> int:
-	return -1
-
-#endregion
-
 @export_category("Family")
 
 @export var offset : Node3D
@@ -76,27 +25,22 @@ static func request_new_bubble_id() -> int:
 @export var padding_container : MarginContainer
 @export var typewriter : Typewriter
 
-@export var control_text : String :
+@export_multiline var control_text : String :
 	get: return typewriter.rtl.text
 	set(value):
 		if not typewriter: return
 		typewriter.rtl.text = value
 
+# @export var control_size : Vector2
 
-# func _ready() -> void:
-
-# 	refresh.call_deferred()
-
-static func _static_init() -> void:
-	rebuild_viewport_cache()
-	# if not _viewport_cache: rebuild_viewport_cache()
+func _ready() -> void:
+	clone_material()
 
 
-func _process(delta: float) -> void:
-	var viewport_image := viewport.get_texture().get_image()
-	var image := viewport_cache.get_layer_data(bubble_id)
-	image.blit_rect(viewport_image, Rect2i(Vector2i.ZERO, viewport_image.get_size()), Vector2i.ZERO)
-	viewport_cache.update_layer(image, bubble_id)
+func clone_material() -> void:
+	text_mesh.mesh.surface_set_material(0, text_mesh.mesh.surface_get_material(0).duplicate())
+	(text_mesh.mesh.surface_get_material(0) as StandardMaterial3D).albedo_texture = viewport.get_texture()
+	# (text_mesh.mesh.surface_get_material(0) as ShaderMaterial).set_shader_parameter(&"albedo_texture", viewport.get_texture())
 
 
 func refresh() -> void:
@@ -111,8 +55,9 @@ func refresh() -> void:
 
 	var size := Vector2(
 		size_curve_x.sample_baked(total_characters),
-		size_curve_y.sample_baked(total_lines)
+		size_curve_y.sample_baked(total_characters)
 	)
+	# var size := control_size
 
 	superegg.size.x = size.x + padding.x
 	superegg.size.y = size.y + padding.y
@@ -121,10 +66,101 @@ func refresh() -> void:
 	offset.position.y = superegg.size.y * 0.5
 
 	viewport.size = text_mesh.mesh.size * viewport_pixels_per_unit
-	text_mesh.set_instance_shader_parameter(&"viewport_ratio", Vector2(viewport.size) / float(_max_viewport_size))
 
 	var margin_override : Vector2i = floor(padding * float(viewport_pixels_per_unit) * 0.5)
 	padding_container.add_theme_constant_override(&"margin_left", margin_override.x)
 	padding_container.add_theme_constant_override(&"margin_right", margin_override.x)
 	padding_container.add_theme_constant_override(&"margin_top", margin_override.y)
 	padding_container.add_theme_constant_override(&"margin_bottom", margin_override.y)
+
+
+# #region Static Viewport Data
+# @export_subgroup("Static")
+
+# static var viewport_cache : Texture2DArray :
+# 	get: return RenderingServer.global_shader_parameter_get(&"bubble_cache")
+# 	set(value):
+# 		RenderingServer.global_shader_parameter_set(&"bubble_cache", value)
+
+# static var _viewports_used : int
+# @export var viewports_used : int :
+# 	get: return _viewports_used
+
+# static var _max_viewport_size : int = 2048
+# @export var max_viewport_size : int = 2048 :
+# 	get: return _max_viewport_size
+# 	set(value):
+# 		if _max_viewport_size == value: return
+# 		_max_viewport_size = value
+# 		rebuild_viewport_cache()
+
+# static var _max_bubbles : int = 8
+# @export_range(0, 63) var max_bubbles : int = 8 :
+# 	get: return _max_bubbles
+# 	set(value):
+# 		if _max_bubbles == value: return
+# 		_max_bubbles = value
+# 		rebuild_viewport_cache()
+
+# static func create_viewport_cache_layer() -> Image:
+# 	return Image.create_empty(_max_viewport_size, _max_viewport_size, false, Image.FORMAT_RGBA8)
+
+# static func rebuild_viewport_cache() -> void:
+# 	var images : Array[Image] = []
+# 	images.resize(_max_bubbles)
+# 	for i in _max_bubbles:
+# 		images[i] = create_viewport_cache_layer()
+# 	viewport_cache = Texture2DArray.new()
+# 	viewport_cache.create_from_images(images)
+
+# var _bubble_id : int = -1
+# @export var bubble_id : int = -1 :
+# 	get: return _bubble_id
+# 	set(value):
+# 		value = clampi(value, -1, _max_bubbles - 1)
+# 		if _bubble_id == value: return
+# 		_bubble_id = value
+# 		if _bubble_id == -1:
+# 			push_warning("Attempted to assign a bubble to a new id, but no more are available.")
+# 			return
+# 		if not text_mesh: return
+# 		text_mesh.set_instance_shader_parameter(&"bubble_id", _bubble_id)
+
+# static func request_new_bubble_id() -> int:
+# 	for i in _max_bubbles:
+# 		if _viewports_used & (1 << i) != 0: continue
+# 		_viewports_used |= (1 << i)
+# 		return i
+# 	return -1
+
+# static func clear_bubble_id(idx : int) -> void:
+# 	_viewports_used &= ~(1 << idx)
+
+
+
+# func _ready() -> void:
+
+# 	refresh.call_deferred()
+
+# static func _static_init() -> void:
+# 	if not viewport_cache: rebuild_viewport_cache()
+
+
+# func _ready() -> void:
+# 	bubble_id = request_new_bubble_id()
+
+
+# func _exit_tree() -> void:
+# 	clear_bubble_id(bubble_id)
+
+
+# func _process(delta: float) -> void:
+# 	refresh()
+
+# 	var viewport_image := viewport.get_texture().get_image()
+# 	var image := viewport_cache.get_layer_data(bubble_id)
+# 	image.blit_rect(viewport_image, Rect2i(Vector2i.ZERO, viewport_image.get_size()), Vector2i.ZERO)
+# 	viewport_cache.update_layer(image, bubble_id)
+# 	text_mesh.set_instance_shader_parameter(&"viewport_ratio", Vector2(viewport.size) / float(_max_viewport_size))
+
+# #endregion
